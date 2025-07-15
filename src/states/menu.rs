b434087d1c927dc50e9b97906327d1e219e56159
Jiangfeng Huang <paraworker@gmail.com>
2025-07-15 19:41:34 +0100

    Add menu background

--- src/states/menu.rs
+++ src/states/menu.rs
@@ -1,9 +1,15 @@
 use crate::states::GameState;
 use bevy::prelude::*;
 
-const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
-const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
-const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.35, 0.35);
+const BUTTON_NORMAL: Color = Color::srgba(0.15, 0.15, 0.15, 0.6);
+const BUTTON_HOVERED: Color = Color::srgba(0.55, 0.55, 0.55, 0.6);
+const BUTTON_PRESSED: Color = Color::srgba(1.0, 1.0, 1.0, 1.0);
+
+const BUTTON_BORDER_NORMAL: Color = Color::srgba(0.0, 0.0, 0.0, 0.9);
+const BUTTON_BORDER_HOVERED: Color = Color::srgba(1.0, 1.0, 1.0, 1.0);
+const BUTTON_BORDER_PRESSED: Color = Color::srgba(1.0, 1.0, 1.0, 1.0);
+
+const BG_PATH: &str = "textures/menu_bg.png";
 
 pub struct MenuPlugin;
 
@@ -28,29 +34,46 @@ fn update(
     for (interaction, mut color, mut border_color) in &mut interaction_query {
         match *interaction {
             Interaction::Pressed => {
-                *color = PRESSED_BUTTON.into();
-                border_color.0 = Color::WHITE;
+                *color = BUTTON_PRESSED.into();
+                border_color.0 = BUTTON_BORDER_PRESSED;
 
                 next_state.set(GameState::GameSetup);
             }
             Interaction::Hovered => {
-                *color = HOVERED_BUTTON.into();
-                border_color.0 = Color::WHITE;
+                *color = BUTTON_HOVERED.into();
+                border_color.0 = BUTTON_BORDER_HOVERED;
             }
             Interaction::None => {
-                *color = NORMAL_BUTTON.into();
-                border_color.0 = Color::BLACK;
+                *color = BUTTON_NORMAL.into();
+                border_color.0 = BUTTON_BORDER_NORMAL;
             }
         }
     }
 }
 
-fn on_enter(mut commands: Commands) {
+fn on_enter(mut commands: Commands, asset_server: Res<AssetServer>) {
     // camera
     commands.spawn((Camera2d, MenuMarker));
 
     // ui
-    commands.spawn((button("New Game"), MenuMarker));
+    commands
+        .spawn((
+            Node {
+                width: Val::Percent(100.),
+                height: Val::Percent(100.),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            MenuMarker,
+        ))
+        .with_children(|parent| {
+            // button
+            parent.spawn(button("New Game"));
+
+            // background image
+            parent.spawn(background(asset_server.load(BG_PATH)));
+        });
 }
 
 fn on_exit(mut commands: Commands, entities: Query<Entity, With<MenuMarker>>) {
@@ -71,25 +94,42 @@ fn button(text: impl Into<String>) -> impl Bundle + 'static {
         children![(
             Button,
             Node {
-                width: Val::Px(150.0),
-                height: Val::Px(65.0),
+                width: Val::Px(200.0),
+                height: Val::Px(80.0),
                 border: UiRect::all(Val::Px(5.0)),
                 justify_content: JustifyContent::Center,
                 align_items: AlignItems::Center,
                 ..default()
             },
-            BorderColor(Color::BLACK),
+            BorderColor(BUTTON_BORDER_NORMAL),
             BorderRadius::all(Val::Px(12.0)),
-            BackgroundColor(NORMAL_BUTTON),
+            BackgroundColor(BUTTON_NORMAL),
             children![(
                 Text::new(text),
                 TextFont {
-                    font_size: 22.0,
+                    font_size: 28.0,
                     ..default()
                 },
                 TextColor(Color::srgb(0.9, 0.9, 0.9)),
                 TextShadow::default(),
             )]
         )],
+    )
+}
+
+fn background(image: Handle<Image>) -> impl Bundle + 'static {
+    (
+        Node {
+            position_type: PositionType::Absolute,
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            top: Val::Px(0.0),
+            left: Val::Px(0.0),
+            right: Val::Px(0.0),
+            bottom: Val::Px(0.0),
+            ..default()
+        },
+        ImageNode { image, ..default() },
+        GlobalZIndex(-1),
     )
 }
